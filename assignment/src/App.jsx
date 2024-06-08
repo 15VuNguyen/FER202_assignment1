12ab5ec0a29c43c2fc480ea741ce6ae45a300cb6
zunguyen1505@gmail.com <zunguyen1505@gmail.com>
2024-06-08 13:24:58 +0700

    done again 1

--- assignment/src/App.jsx
+++ assignment/src/App.jsx
@@ -3,7 +3,6 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
 import React, { useState } from "react";
-import Login from "./components/Login";
 import User from "./components/User";
 import Admin from "./components/Admin";
 import { CourseContextProvider } from "./components/context/courseContext";
@@ -15,7 +14,6 @@ function App() {
       <div className="app">
         <CourseContextProvider>
           <Routes>
-            {/* <Route path="/" element={<Login />} /> */}
             <Route path="/" element={<User />} />
             <Route path="/Admin" element={<Admin />} />
           </Routes>
